57b5bde06fae90225ab220ba6ae56e280f311c90
SNK047 <niroshkumar.0210@gmail.com>
2026-04-26 21:12:51 +0530

    Remove next-pwa config to fix Vercel build error

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Add these only if you really need them later
-  // images: {
-  //   domains: ['example.com'],
-  // },
 };
 
 export default nextConfig;
